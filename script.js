bce3db5d6fec072ee6d935f1f4cef4bad3efdae7
Aleksey <lexa.11@list.ru>
2020-12-10 18:59:31 +0300

    Added

---
+++ script.js
@@ -0,0 +1,20 @@
+const sum = (a1, a2) => {
+    let s = a1 + a2; 
+    console.log (s);
+}
+sum('viski', ' rom');
+sum('kola', ' rom');
+sum(5, ' rums');
+
+let number = 5;
+if (number > 0){
+    let newvar = 20;
+} else{
+    var oldvar = 10;
+}
+console.log (oldvar);
+
+let Array = ['hi', 2.5, 3, true];
+for (let i = 0; i < Array.length; i++) {
+    console.log (Array[i]);
+}
